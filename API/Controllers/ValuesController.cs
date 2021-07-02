79a91fa4e39c2911244432b9364cc090658edba0
Mohammad Mushfique <m.mushfique07@gmail.com>
2021-07-02 09:46:04 +0530

    First Commit in Git

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+       
+        public HttpResponseMessage GetMessages()
+        {
+            List<demo_tushar> msgList = new List<demo_tushar>();
+            using (TusharDbEntities db = new TusharDbEntities())
+            {
+                msgList = db.demo_tushars.ToList();
+            }
+            HttpResponseMessage response;
+            response = Request.CreateResponse(HttpStatusCode.OK, msgList);
+
+            return response;
+
+
+        }
+
+    }
+}
